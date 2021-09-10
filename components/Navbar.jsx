f06b0ff40387d205e77df2a7db804ad17a270d26
anthonybartczak <to0oni456@gmail.com>
2021-09-10 09:37:00 +0200

    Projects page 404 fix v5

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -38,8 +38,8 @@ export const Navbar = () => {
                 <a onClick={() => scrollToID("home")} className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Home</a>
                 <a onClick={() => scrollToID("timeline")} className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Timeline</a>
                 <a onClick={() => scrollToID("tech-stack")} className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Tech</a>
-                <Link href={"/projects"} as={"/projects"}><a className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Projects</a></Link>
-                <Link href={"/contact"} as={"/contact"}><a className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Contact</a></Link>
+                <Link href={"/projects"} as={"/projects"} passHref><a className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Projects</a></Link>
+                <Link href={"/contact"} as={"/contact"} passHref><a className="font-abel text-lg py-3 px-6 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Contact</a></Link>
               </div>
             </div>
             <div className="md:hidden flex items-center">
@@ -64,8 +64,8 @@ export const Navbar = () => {
             <li><a onClick={() => scrollToID("home")} className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Home</a></li>
             <li><a onClick={() => scrollToID("timeline")} className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Timeline</a></li>
             <li><a onClick={() => scrollToID("tech-stack")} className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Tech</a></li>
-            <Link href="/projects" ><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Projects</a></li></Link>
-            <Link href="/contact"><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Contact</a></li></Link>
+            <Link href={"/contact"} as={"/contact"} passHref><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Projects</a></li></Link>
+            <Link href={"/contact"} as={"/contact"} passHref><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Contact</a></li></Link>
           </ul>
         </div>
       </nav>
