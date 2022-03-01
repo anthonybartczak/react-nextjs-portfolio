f878687000aca3229d3d12d83a306293165354d5
anthonybartczak <to0oni456@gmail.com>
2022-03-01 15:36:42 +0100

    Minor nav change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -64,7 +64,7 @@ export const Navbar = () => {
             <div className="flex space-x-16">
               <div className="hidden md:flex items-center space-x-1">
                 <Link href={"/"} as={"/"} passHref><a className="font-abel text-lg py-3 px-6 dark:text-gray-50 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Home</a></Link>
-                <Link href={"/projects"} as={"/projects"} passHref><a className="font-abel text-lg py-3 px-6 dark:text-gray-50 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Projects</a></Link>
+                <Link href={"/projects"} as={"/projects"} passHref><a className="font-abel text-lg py-3 px-6 dark:text-gray-50 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Github</a></Link>
                 <Link href={"/contact"} as={"/contact"} passHref><a className="font-abel text-lg py-3 px-6 dark:text-gray-50 text-gray-500 font-semibold hover:text-gray-700 transition duration-300 border-b-2 hover:border-green-400">Contact</a></Link>
               </div>
             </div>
@@ -88,7 +88,7 @@ export const Navbar = () => {
         <div className="hidden mobile-menu">
           <ul className="">
             <Link href={"/"} as={"/"} passHref><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Home</a></li></Link>
-            <Link href={"/projects"} as={"/projects"} passHref><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Projects</a></li></Link>
+            <Link href={"/projects"} as={"/projects"} passHref><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Github</a></li></Link>
             <Link href={"/contact"} as={"/contact"} passHref><li><a className="font-abel subpixel-antialiased text-md text-center font-bold block px-2 py-4 hover:bg-green-500 transition duration-300">Contact</a></li></Link>
           </ul>
         </div>
