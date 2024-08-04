be38ab62a57d2253c1d8fe02bce87f461e12c9d1
anthonybartczak <to0oni456@gmail.com>
2024-08-04 14:33:52 +0200

    GTM fix v2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,24 +20,24 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MKHGC28F');</script>
+              })(window,document,'script','dataLayer','GTM-MKHGC28F');
               `,
             }}
           ></script>
         </Head>
         <body>
-          <script
+          <noscript
             dangerouslySetInnerHTML={{
               __html: `
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKHGC28F"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKHGC28F"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
               `,
             }}
-          ></script>
+          ></noscript>
           <script
             dangerouslySetInnerHTML={{
               __html: `
