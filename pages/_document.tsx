09255aa244eca41390d3a416a798644ab7179dde
Antoni Bartczak <49080651+anthonybartczak@users.noreply.github.com>
2024-08-04 14:27:52 +0200

    GTM code fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,13 +20,11 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              <!-- Google Tag Manager -->
               <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-MKHGC28F');</script>
-              <!-- End Google Tag Manager -->
               `,
             }}
           ></script>
@@ -35,10 +33,8 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              <!-- Google Tag Manager (noscript) -->
               <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKHGC28F"
               height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-              <!-- End Google Tag Manager (noscript) -->
               `,
             }}
           ></script>
